3cbb3d366563cbd65a8fb1460fd1bce454c40f11
Tomek Rozalski <alium@daucusetanser.pl>
2019-03-21 22:19:26 +0100

    ESLint and JSDoc configuration. Research how to lint project. I thought about Prettier, but for now IMHO ESLint also fix code on save correctly

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "extends": [
+        "react-app",
+        "airbnb",
+    ],
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "jsdoc"
+    ],
+    "rules": {
+        "indent": [2, "tab"],
+        "no-tabs": 0,
+        "react/jsx-indent": [2, 'tab'],
+        "react/jsx-indent-props": [2, 'tab'],
+        // insert react rules here
+        "jsdoc/check-examples": 1,
+        "jsdoc/check-param-names": 1,
+        "jsdoc/check-tag-names": 1,
+        "jsdoc/check-types": 1,
+        "jsdoc/newline-after-description": 1,
+        "jsdoc/no-undefined-types": 1,
+        "jsdoc/require-description": 0,
+        "jsdoc/require-description-complete-sentence": 1,
+        "jsdoc/require-example": 0,
+        "jsdoc/require-hyphen-before-param-description": 1,
+        "jsdoc/require-param": 1,
+        "jsdoc/require-param-description": 1,
+        "jsdoc/require-param-name": 1,
+        "jsdoc/require-param-type": 1,
+        "jsdoc/require-returns": 1,
+        "jsdoc/require-returns-check": 1,
+        "jsdoc/require-returns-description": 1,
+        "jsdoc/require-returns-type": 1,
+        "jsdoc/valid-types": 1
+    }
+}
