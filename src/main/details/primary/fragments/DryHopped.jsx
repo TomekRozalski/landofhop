92c32ccfe3343a2ec3a62a76ee45ca4e9dc3b4bd
Tomek Rozalski <alium@daucusetanser.pl>
2019-05-16 19:40:54 +0200

    [Details] Fix dryHopped display after last db schema updates

--- src/main/details/primary/fragments/DryHopped.jsx
+++ src/main/details/primary/fragments/DryHopped.jsx
@@ -1,13 +1,38 @@
+/* eslint-disable no-shadow */
 import React, { useContext } from 'react';
-import { get, isBoolean } from 'lodash';
+import styled from 'styled-components';
+import {
+	get,
+	isArray,
+	isBoolean,
+	isUndefined,
+} from 'lodash';
 import { FormattedMessage } from 'react-intl';
 
-import { BeverageDetailsContext } from 'config';
+import { BeverageDetailsContext, LanguageContext } from 'config';
 import { constants } from 'utils';
+import { getNameByLanguage } from 'utils/helpers';
 import { DT, DD, Highlight } from 'elements';
+
+const HopList = styled.ul`
+	display: inline;
+
+	::before {
+		content: ' (';
+	}
+
+	::after {
+		content: ')';
+	}
+
+	li {
+		display: inline;
+	}
+`;
 
 const DryHopped = () => {
 	const { beverage } = useContext(BeverageDetailsContext);
+	const { language } = useContext(LanguageContext);
 
 	const labelValue = get(beverage, 'label.brewing.dryHopped');
 	const producerValue = get(beverage, 'producer.brewing.dryHopped');
@@ -23,12 +48,12 @@ const DryHopped = () => {
 
 	const formattedValues = values
 		.reduce((acc, curr) => {
-			if (!acc.length && isBoolean(curr.value)) {
+			if (!acc.length && !isUndefined(curr.value)) {
 				return [curr];
 			}
 
 			return (
-				isBoolean(curr.value)
+				!isUndefined(curr.value)
 					? [...acc, separators.section, curr]
 					: acc
 			);
@@ -38,11 +63,47 @@ const DryHopped = () => {
 				return item;
 			}
 
-			return (
-				<Highlight key={item.type} type={item.type}>
-					<FormattedMessage id={`details.${item.value ? 'yes' : 'no'}`} />
-				</Highlight>
-			);
+			if (isBoolean(item.value)) {
+				return (
+					<Highlight key={item.type} type={item.type}>
+						<FormattedMessage id={`details.${item.value ? 'yes' : 'no'}`} />
+					</Highlight>
+				);
+			}
+
+			if (isArray(item.value)) {
+				const typeArray = item.value
+					.reduce((acc, curr) => {
+						const formattedName = getNameByLanguage({ values: curr.name, language });
+
+						return (
+							acc.length
+								? [...acc, separators.item, formattedName]
+								: [formattedName]
+						);
+					}, [])
+					.map(item => (
+						item === separators.item
+							? item
+							: (
+								<li
+									key={item.value}
+									lang={item.language === language ? null : item.language}
+								>
+									{item.value}
+								</li>
+							)
+					));
+
+				return (
+					<Highlight key={item.type} type={item.type}>
+						<FormattedMessage id="details.yes" />
+						<HopList>{ typeArray }</HopList>
+					</Highlight>
+				);
+			}
+
+			return null;
 		});
 
 	return formattedValues.length ? (
