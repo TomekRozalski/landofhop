95e647f4c2143f9f5400e9786a53101edcb4adac
Tomek Rozalski <alium@daucusetanser.pl>
2019-08-27 21:05:02 +0200

    [Tiles] Minor fixes

--- src/elements/ContentWrapper.jsx
+++ src/elements/ContentWrapper.jsx
@@ -8,7 +8,7 @@ const Main = styled.main`
 	min-height: 100vh;
 	border: 3rem solid ${colors.gray[100]};
 	border-top: 0;
-	padding: ${sizes.header.height.tall.lg} 0 8rem 0;
+	padding: calc(${sizes.header.height.tall.lg} + 10px) 0 8rem 0;
 	background-color: ${colors.gray[700]};
 	position: relative;
 	z-index: ${indexes.main};
